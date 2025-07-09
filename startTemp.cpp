184a3b4838d27951e76b028498ee4fec330914fb
Mohammed Ibrahim <163002011+Mohamediibra7im@users.noreply.github.com>
2025-07-09 09:04:26 +0300

    Initial commit of startTemp.cpp with basic setup and I/O functions

---
+++ startTemp.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef double db;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<db> vd;
+typedef vector<ld> vld;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+typedef vector<pii> vii;
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define endl '\n'
+#define time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << endl;
+//!=========================>>Fast-IO-Functions<<=================
+void fastIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+//!=========================>>File-IO-Functions<<=================
+void fileIO()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+//!=========================>>Main-Code<<=========================
+int main()
+{
+    fileIO();
+    fastIO();
+
+    // Enter your code here
+
+    return 0;
+}
